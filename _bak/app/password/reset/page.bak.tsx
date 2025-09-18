3743f3499ac973a735b37b743ef51a342285f964
Akamatsu Daichi <daichi@AkamatsunoMacBook-Pro.local>
2025-09-18 16:50:23 +0900

    chore: remove _bak from repo and ignore backup folder

--- _bak/app/password/reset/page.bak.tsx
+++
@@ -1,9 +0,0 @@
-import ResetPasswordForm from "./ResetPasswordForm";
-
-export const metadata = {
-  title: "新しいパスワードの設定 | richpass",
-};
-
-export default function Page() {
-  return <ResetPasswordForm />;
-}
